2fd147dc24590206f3a5ed3f351ee55926ca569d
Allie <allie@codefellows.com>
2019-01-08 17:46:06 -0800

    Adding day 2 demos

---
+++ 02-jquery-selectors-events/demos/jquery-selectors/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+$('.first').text();
+
+$('#list').append('<li>301 is awesome!</li>');
+$('#list').append('<li>301 is super awesome!</li>');
--- 02-jquery-selectors-events/demos/read-json/app.js
+++ 02-jquery-selectors-events/demos/read-json/app.js
@@ -1,26 +1,41 @@
 'use strict';
 
 function Dog(dogObject) {
-
+  this.name = dogObject.name;
+  this.image_url = dogObject.image_url;
+  this.hobbies = dogObject.hobbies;
 }
 
 Dog.allDogs = [];
 
 Dog.prototype.render = function() {
-  // making a copy of the template
   // make a new section
- 
+  $('main').append('<section class="clone"></section>');
+  const $dogClone = $('section[class="clone"]');
 
   // get the html of the template
+  const $dogHtml = $('#dog-template').html();
 
   // set the section's html === template html
+  $dogClone.html( $dogHtml );
 
   // fill in the properties from each instance
-
+  $dogClone.find('h2').text(this.name);
+  $dogClone.find('img').attr('src', this.image_url);
+  $dogClone.find('p').text(this.hobbies);
+  $dogClone.removeClass('clone');
+  $dogClone.addClass(this.name);
 };
 
 Dog.readJson = () => {
-
+  $.get('data.json', 'json')
+    .then(data => {
+      console.log(data);
+      data.forEach(dog => {
+        Dog.allDogs.push( new Dog(dog) );
+      });
+    })
+    .then(Dog.loadDogs);
 };
 
 Dog.loadDogs = () => {
