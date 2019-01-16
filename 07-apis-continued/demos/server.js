d99fc1c18c4e77b04ac116b8324e7a6917f07940
Allie <allie@codefellows.com>
2019-01-15 17:17:09 -0800

    Adds lecture demo

--- 07-apis-continued/demos/server.js
+++ 07-apis-continued/demos/server.js
@@ -3,57 +3,80 @@
 // application dependencies
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 // configure environment variables
 require('dotenv').config();
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get('/location', (request, response) => {
-  const locationData = searchToLatLong(request.query.data);
-  response.send(locationData);
-});
+// app.get('/location', (request, response) => {
+//   const locationData = searchToLatLong(request.query.data);
+//   response.send(locationData);
+// });
 
-app.get('/weather', (request, response) => {
-  const weatherData = getWeather();
-  response.send(weatherData);
-});
+app.get('/location', searchToLatLong);
+app.get('/weather', getWeather);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+// ERROR HANDLING
+function handleError(error, response) {
+  console.error(error);
+  if(response) response.status(500).send('Sorry, something went wrong!');
+}
 
 
 // HELPER FUNCTIONS
 
 // Solution from day 6
-function searchToLatLong(query) {
-  const geoData = require('./data/geo.json');
-  const location = new Location(geoData);
-  location.search_query = query;
-  console.log(location);
-  return location;
+// function searchToLatLong(query) {
+//   const geoData = require('./data/geo.json');
+//   const location = new Location(geoData);
+//   location.search_query = query;
+//   console.log(location);
+//   return location;
+// }
+
+// refactored function
+function searchToLatLong(request, response) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+
+  return superagent.get(url)
+    .then(apiResponse => {
+      let location = new Location(request.query.data, apiResponse);
+      response.send(location);
+
+      // response.send(new Location(request.query.data, apiResponse));
+    })
+    .catch(error => handleError(error, response));
 }
 
-function getWeather() {
-  const darkskyData = require('./data/darksky.json');
+function getWeather(request, response) {
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
 
-  const weatherSummaries = [];
+  return superagent.get(url)
+    .then(weatherResponse => {
 
-  darkskyData.daily.data.forEach(day => {
-    weatherSummaries.push(new Weather(day));
-  });
+      const weatherSummaries = weatherResponse.body.daily.data.map(day => {
+        return new Weather(day);
+      });
 
-  return weatherSummaries;
+      response.send(weatherSummaries);
+    })
+    .catch(error => handleError(error, response));
 }
 
-function Location(query, res) {
+function Location(query, apiResult) {
   this.search_query = query;
-  this.formatted_query = res.body.results[0].formatted_address;
-  this.latitude = res.body.results[0].geometry.location.lat;
-  this.longitude = res.body.results[0].geometry.location.lng;
+  this.formatted_query = apiResult.body.results[0].formatted_address;
+  this.latitude = apiResult.body.results[0].geometry.location.lat;
+  this.longitude = apiResult.body.results[0].geometry.location.lng;
 }
 
 function Weather(day) {
---
+++ 07-apis-continued/repl.js
@@ -0,0 +1,56 @@
+const nums = [1, 2, 3, 4, 5];
+
+// nums.map(num => num * num);
+// nums.map(num => {
+//   return num * num;
+// });
+
+const characters = [
+  {
+    name: 'Eddard',
+    spouse: 'Catelyn',
+    children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+    house: 'Stark'
+  },
+  {
+    name: 'Jon A.',
+    spouse: 'Lysa',
+    children: ['Robin'],
+    house: 'Arryn'
+  },
+  {
+    name: 'Cersei',
+    spouse: 'Robert',
+    children: ['Joffrey', 'Myrcella', 'Tommen'],
+    house: 'Lannister'
+  },
+  {
+    name: 'Daenarys',
+    spouse: 'Khal Drogo',
+    children: ['Drogon', 'Rhaegal', 'Viserion'],
+    house: 'Targaryen'
+  },
+  {
+    name: 'Mace',
+    spouse: 'Alerie',
+    children: ['Margaery', 'Loras'],
+    house: 'Tyrell'
+  },
+  {
+    name: 'Sansa',
+    spouse: 'Tyrion',
+    children: [],
+    house: 'Stark'
+  },
+  {
+    name: 'Jon S.',
+    spouse: null,
+    children: [],
+    house: 'Snow'
+  }
+]
+
+characters.map((character, idx) => {
+  console.log('idx', idx);
+  return character.name;
+})
